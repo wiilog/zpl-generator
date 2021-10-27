acecf0611e807a86b10955751786f2ce7d38fcc3
Marwane <marwane.benoukaiss@wiilog.fr>
2021-10-27 15:32:57 +0200

    Add dimensions getter in TextConfig

--- src/Elements/Text/TextConfig.php
+++ src/Elements/Text/TextConfig.php
@@ -38,30 +38,32 @@ class TextConfig implements ToZPL {
     }
 
     public function toZPL(Context $context) {
-        $context->a($this->font . $this->rotation, ...$this->dimensions($context));
+        [$x, $y] = $this->dimensions();
+
+        $context->a($this->font . $this->rotation, $context->toDots(Context::X, $x), $context->toDots(Context::Y, $y));
     }
 
-    private function dimensions(Context $context): array {
+    public function dimensionsOf(string $text): array {
+        [$width, $height] = $this->dimensions();
+        return [$width * strlen($text), $height];
+    }
+
+    public function dimensions(): array {
         if($this->font === Font::ZEBRA_DEFAULT) {
-            [$x, $y] = [$this->size, $this->size];
+            return [$this->size, $this->size];
         } else if($this->font === Font::ZEBRA_A) {
-            [$x, $y] = [$this->size, $this->size * 0.5];
+            return [$this->size, $this->size * 0.5];
         } else if($this->font === Font::ZEBRA_B) {
-            [$x, $y] = [$this->size, $this->size * 0.7];
+            return [$this->size, $this->size * 0.7];
         } else if($this->font === Font::ZEBRA_C) {
-            [$x, $y] = [$this->size, $this->size * 0.5];
+            return [$this->size, $this->size * 0.5];
         } else if($this->font === Font::ZEBRA_D) {
-            [$x, $y] = [$this->size, $this->size * 0.5];
+            return [$this->size, $this->size * 0.5];
         } else if($this->font === Font::ZEBRA_F) {
-            [$x, $y] = [$this->size, $this->size * 0.5];
+            return [$this->size, $this->size * 0.5];
         } else {
             throw new RuntimeException("Unknown font \"$this->font\"");
         }
-
-        return [
-            $context->toDots(Context::X, $x),
-            $context->toDots(Context::Y, $y),
-        ];
     }
 
 }
